1511e63c4342f26744ebe86e15f606508cadbf3e
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-12 23:51:06 +0500

    feat: implement message controller and routes for sending messages

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,15 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import Message from "../models/message.model.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const sendMessage = asyncHandler(async (req, res, next) => {
+  const { senderName, subject, message } = req.body;
+  if (!senderName || !subject || !message) {
+    return next(new ApiError(400, "Please provide all fields"));
+  }
+  const data = await Message.create({ senderName, subject, message });
+  res.status(201).json(new ApiResponse(201, data));
+});
+
+export { sendMessage };
