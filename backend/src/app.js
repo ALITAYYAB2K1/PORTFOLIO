179db0bcfe3e534f539758cd054ed7e3a92a76d4
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-12 19:30:09 +0500

    feat: update environment configuration and enhance CORS settings; add MongoDB connection utility

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,13 @@ import cors from "cors";
 import cookieparser from "cookie-parser";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(cookieparser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
