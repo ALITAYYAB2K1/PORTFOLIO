110599da5ff68207eebd396c31443e016e1af1ff
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-13 11:30:39 +0500

    error handle for default export in message model and controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import Message from "../models/message.model.js";
+import { Message } from "../models/message.model.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
