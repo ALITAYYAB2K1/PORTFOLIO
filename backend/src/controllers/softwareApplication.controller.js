101043e5fac0a3c746acb1b2331d868ca774530f
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-15 14:58:56 +0500

    feat: implement getAllApplication and deleteApplication functions in softwareApplication.controller

--- backend/src/controllers/softwareApplication.controller.js
+++ backend/src/controllers/softwareApplication.controller.js
@@ -3,7 +3,8 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { SoftwareApplication } from "../models/softwareApplication.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-
+import { v2 as cloudinary } from "cloudinary";
+import { image_ID_Parser } from "../utils/imageParser.js";
 const addNewApplication = asyncHandler(async (req, res, next) => {
   const { name } = req.body;
   if (!name) {
@@ -24,7 +25,27 @@ const addNewApplication = asyncHandler(async (req, res, next) => {
   res.status(201).json(new ApiResponse("Application created", newApplication));
 });
 
-const getAllApplication = asyncHandler(async (req, res, next) => {});
-const deleteApplication = asyncHandler(async (req, res, next) => {});
+const getAllApplication = asyncHandler(async (req, res, next) => {
+  const applications = await SoftwareApplication.find({});
+  if (!applications || applications.length === 0) {
+    throw new ApiError("Applications not found", 404);
+  }
+  res.status(200).json(new ApiResponse("Applications found", applications));
+});
+
+const deleteApplication = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const application = await SoftwareApplication.findById(id);
+  if (!application) {
+    throw new ApiError("Application not found", 404);
+  }
+  const prevSvg = application.svg;
+  const publicId = image_ID_Parser(prevSvg);
+  console.log(`Attempting to delete avatar with public ID: ${publicId}`);
+  const result = await cloudinary.uploader.destroy(publicId);
+  console.log(result, "deleted svg from cloudinary");
+  await application.deleteOne();
+  res.status(200).json(new ApiResponse("Application deleted", null));
+});
 
 export { addNewApplication, getAllApplication, deleteApplication };
