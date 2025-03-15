ab74bc0069e030a965a1efd34b84f71b94fdc0b1
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-15 16:31:33 +0500

    project routes

---
+++ backend/src/routes/project.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  addNewProject,
+  getAllProject,
+  deleteProject,
+  updateProject,
+  getProject,
+} from "../controllers/project.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router();
+//secure routes
+router
+  .route("/add")
+  .post(
+    verifyJWT,
+    upload.fields([{ name: "image", maxCount: 1 }]),
+    addNewProject
+  );
+router.route("/getall").get(verifyJWT, getAllProject);
+router.route("/delete/:id").delete(verifyJWT, deleteProject);
+router.route("/update/:id").put(verifyJWT, updateProject);
+router.route("/get/:id").put(verifyJWT, getProject);
+
+export default router;
