f11d577dd7474e490bb9a7a86739937a8ccf221e
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-15 20:28:34 +0500

    dashboard starting

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh bg-amber-400">
-      <Button className="bg-amber-900">Click me</Button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" />
+        <Route path="/login" />
+        <Route path="/password/forgot" />
+        <Route path="/password/reset/:token" />
+        <Route path="/manage/skills" />
+        <Route path="/manage/timeline" />
+        <Route path="/manage/projects" />
+        <Route path="/view/project/:id" />
+        <Route path="/update/project/:id" />
+      </Routes>
+    </Router>
   );
 }
 
