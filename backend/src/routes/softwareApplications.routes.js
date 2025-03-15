feced4db00a30b177832ccf06854308a22a0f9e6
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-15 13:09:55 +0500

    add middleware to softwareapplication routes

--- backend/src/routes/softwareApplications.routes.js
+++ backend/src/routes/softwareApplications.routes.js
@@ -3,12 +3,19 @@ import {
   addNewApplication,
   getAllApplication,
   deleteApplication,
-} from "../controllers/timeline.controller.js";
+} from "../controllers/softwareApplication.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 //secure routes
-router.route("/add").post(verifyJWT, addNewApplication);
+router
+  .route("/add")
+  .post(
+    verifyJWT,
+    upload.fields([{ name: "svg", maxCount: 1 }]),
+    addNewApplication
+  );
 router.route("/getall").get(verifyJWT, getAllApplication);
 router.route("/delete/:id").delete(verifyJWT, deleteApplication);
 
