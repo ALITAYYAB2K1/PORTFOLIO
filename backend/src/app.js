094c576cfefb815d95924c8bb62b851ff2d6ace9
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-15 12:24:48 +0500

    feat: add timeline creation functionality in the timeline controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,8 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieparser());
+
 import messageRouter from "./routes/message.routes.js";
 import userRouter from "./routes/user.routes.js";
+import timelineRouter from "./routes/timeline.routes.js";
 app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/timeline", timelineRouter);
 export default app;
--- backend/src/controllers/timeline.controller.js
+++ backend/src/controllers/timeline.controller.js
@@ -1,8 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import { Timeline } from "../models/timeline.model.js";
 
-const addTimeline = asyncHandler(async (req, res, next) => {});
+const addTimeline = asyncHandler(async (req, res, next) => {
+  const { title, description, from, to } = req.body;
+  const timeline = await Timeline.create({
+    title,
+    description,
+    timeline: { from, to },
+  });
+  if (!timeline) {
+    return next(new ApiError("Timeline not created", 400));
+  }
+  res
+    .status(201)
+    .json(new ApiResponse("Timeline created successfully", timeline));
+});
 
 const getAllTimelines = asyncHandler(async (req, res, next) => {});
 
