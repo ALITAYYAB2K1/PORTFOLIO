077d6a2462a323b6a24a95963011e80e599505c8
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-15 12:35:59 +0500

    completed and checked timeline controller with postman for verification

--- backend/src/controllers/timeline.controller.js
+++ backend/src/controllers/timeline.controller.js
@@ -18,8 +18,24 @@ const addTimeline = asyncHandler(async (req, res, next) => {
     .json(new ApiResponse("Timeline created successfully", timeline));
 });
 
-const getAllTimelines = asyncHandler(async (req, res, next) => {});
+const getAllTimelines = asyncHandler(async (req, res, next) => {
+  const timelines = await Timeline.find({});
+  if (!timelines) {
+    return next(new ApiError("No timelines found", 404));
+  }
+  res.status(200).json(new ApiResponse("All timelines", timelines));
+});
 
-const deleteTimeline = asyncHandler(async (req, res, next) => {});
+const deleteTimeline = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const timelines = await Timeline.findById(id);
+  if (!timelines) {
+    return next(new ApiError("No timelines found", 404));
+  }
+  await timelines.deleteOne();
+  res
+    .status(200)
+    .json(new ApiResponse("Timeline deleted successfully", timelines));
+});
 
 export { addTimeline, getAllTimelines, deleteTimeline };
