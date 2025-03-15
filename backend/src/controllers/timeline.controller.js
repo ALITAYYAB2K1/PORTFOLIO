9c6d833ae37bec932cca08f2986cbbf7afbe6b9e
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-15 11:32:44 +0500

    feat: implement timeline controller with add, get, and delete functionality

---
+++ backend/src/controllers/timeline.controller.js
@@ -0,0 +1,11 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const addTimeline = asyncHandler(async (req, res, next) => {});
+
+const getAllTimelines = asyncHandler(async (req, res, next) => {});
+
+const deleteTimeline = asyncHandler(async (req, res, next) => {});
+
+export { addTimeline, getAllTimelines, deleteTimeline };
