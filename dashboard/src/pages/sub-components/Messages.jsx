83ecede8ec9d17a639a8cc483e8b0c7c678e5570
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-21 20:49:35 +0500

    mesage.jsx start

--- dashboard/src/pages/sub-components/Messages.jsx
+++ dashboard/src/pages/sub-components/Messages.jsx
@@ -1,5 +1,47 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "../../components/ui/tabs";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "../../components/ui/card";
+import { Button } from "../../components/ui/button";
 export function Messages() {
-  return <div>Messages</div>;
+  const navigateTo = useNavigate();
+
+  const handleReturnToDashboard = () => {
+    navigateTo("/");
+  };
+  //const {} = useSelector((state) => state.Messages);
+  const [messageId, setMessageId] = useState("");
+  return (
+    <>
+      <div className="min-h-[100vh] sm:gap-4 sm:py-4 sm:pl-20">
+        <Tabs>
+          <TabsContent>
+            <Card>
+              <CardHeader className="flex gap-4 sm:justify-between sm:flex-row sm:items-center">
+                <CardTitle>Messages</CardTitle>
+                <Button className="w-fit" onClick={handleReturnToDashboard}>
+                  Return to Dashboard
+                </Button>
+              </CardHeader>
+              <CardContent className="grid sm:grid-cols-2 gap-4"></CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 }
