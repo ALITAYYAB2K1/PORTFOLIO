0aebb2e051075d862e3aeafab00a4f1b6058a18b
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-21 23:26:25 +0500

    add skill management slice and integrate into Redux store; add drawer component for UI

--- dashboard/src/store/store.js
+++ dashboard/src/store/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userReducers } from "./userSlice.js";
 import { forgotPasswordSlice } from "./forgotResetPasswordSlice.js";
 import messagesReducer from "./messageSlice.js";
-import timelineReducer from "./timelineSlice.js"; // Import the timeline reducer
+import timelineReducer from "./timelineSlice.js";
+import skillReducer from "./skillSlice.js"; // Import the skill reducer
 
 export const store = configureStore({
   reducer: {
     user: userReducers,
     forgotPassword: forgotPasswordSlice,
     messages: messagesReducer,
-    timeline: timelineReducer, // Add the timeline reducer
+    timeline: timelineReducer,
+    skill: skillReducer, // Add the skill reducer
   },
 });
